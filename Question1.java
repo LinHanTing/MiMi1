b26cada9ec7f6a503a8575b52b0626b2bdd23e4a
fate3010 <spk124578@yahoo.com.tw>
2015-04-27 11:53:24 +0800

    Update Question1.java

--- Question1.java
+++ Question1.java
@@ -1,7 +1,11 @@
 ﻿public class Question1{
 	public statis void main(String[] args){
-		System.out.println("Test");
-		System.out.println("C revert B");
-		System.out.printfln("C");
+ 		System.out.println("Test");
++		System.out.printf("A");
+-		System.out.printf("A AM012060 林漢庭提交");
+		System.out.printf("B");
+		System.out.printf("A AM012228 賴柏良提交");
+
+ 
 	}
 }
